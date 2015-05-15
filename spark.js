e9e001c0c847e2f6f042ba53b1c078e3325f5939
David Young <ohnoitsyou7043@gmail.com>
2015-05-15 02:41:08 -0400

    Revert "Cleaning up with eslint"
    This reverts commit 80ade6d8d453a48324aa69ad0bd0c7882bf97f14.
    
    Conflicts:
    	spark.js

--- spark.js
+++ spark.js
@@ -1,8 +1,7 @@
-"use strict";
-var express = require("express");
-var spark = require("spark");
-//var util = require("util");
-var debug = require("debug")("spark");
+var express = require('express');
+var spark = require('spark');
+var util = require('util');
+var debug = require('debug')('spark');
 
 var Spark = function() {
   this.version = "0.1.0";
@@ -14,60 +13,50 @@ var Spark = function() {
   this.deviceInterval = "";
   this.deviceRefreshInterval = 10000;
   this.load = function(options) {
-    debug("[Load] Starting");
-    if("sparkAccessToken" in options) {
-      this.accessToken = options.sparkAccessToken;
-    } else if("sparkUsername" in options && "sparkPassword" in options) {
-      this.username = options.sparkUsername;
-      this.password = options.sparkPassword;
+    debug('[Load] Starting');
+    if('sparkAccessToken' in options) {
+      this.accessToken = options['sparkAccessToken'];
+    } else if('sparkUsername' in options && 'sparkPassword' in options) {
+      this.username = options['sparkUsername'];
+      this.password = options['sparkPassword'];
     } else {
-      debug("[Load] No login method provided");
+      debug('[Load] No login method provided');
     }
-    debug("[Load] Finishing");
-  };
+    debug('[Load] Finishing');
+  },
   this.initilize = function() {
-    debug("[Initilize] Starting");
+    debug('[Initilize] Starting');
     _login(this);
     _getDevices(this);
-    this.deviceInterval = setInterval(_getDevices, this.deviceRefreshInterval, this);
-    debug("[Initilize] Finishing");
-  };
+    this.deviceInterval = setInterval(_getDevices,this.deviceRefreshInterval,this);
+    debug('[Initilize] Finishing');
+  },
   this.loadRoutes = function() {
-    debug("[LoadRoutes] Starting");
-    debug("[LoadRoutes] Loading /devices");
-    this.router.get("/devices", function(req, res, next) {
+    debug('[LoadRoutes] Starting');
+    this.router.get('/devices', function(req, res) {
       res.send(JSON.stringify(this.knownDevices));
       next();
     });
-    debug("[LoadRoutes] Loading /sendCommand");
-    this.router.get("/sendCommand", function(req, res, next) {
-      res.send("ok");
-      next();
+    this.router.get('/sendCommand', function(req, res) {
+      res.send('ok');
     });
-    debug("[LoadRoutes] Loading /sendCommand/:device/:command");
-    this.router.get("/sendCommand/:device/:command", function(req, res, next) {
-      res.send("ok");
-      next();
+    this.router.get('/', function(req, res) {
+      res.send('Spark!');
     });
-    debug("[LoadRoutes] Loading /");
-    this.router.get("/", function(req, res, next) {
-      res.send("Spark!");
-      next();
-    });
-    debug("[LoadRoutes] Finishing");
+    debug('[LoadRoutes] Finishing');
     return this.router;
-  };
-};
+  }
+}
 function _login(t) {
-  debug("[Login] Starting");
+  debug('[Login] Starting');
   var loginMethod = {};
-  if(t.accessToken !== "") {
-    loginMethod.accessToken = t.accessToken;
-  } else if(this.username !== "" && t.password !== "") {
-    loginMethod.username = t.username;
-    loginMethod.password = t.password;
+  if(t.accessToken != "") {
+    loginMethod['accessToken'] = t.accessToken;
+  } else if(this.username != "" && t.password != "") {
+    loginMethod['username'] = t.username;
+    loginMethod['password'] = t.password;
   } else {
-    debug("[Login] No auth method supplied");
+    debug('[Login] No auth method supplied');
   }
   return spark.login(loginMethod).then(function(token) {
     t.accessToken = token;
@@ -75,9 +64,9 @@ function _login(t) {
     debug("[Login] Success:");//,token);
     debug("[Login] Finishing");
   }, function(error) {
-    debug("[Login] Error: %s", error);
-    debug("[Login] Finishing");
+    debug('[Login] Error: %s',error);
   });
+  debug('[Login] Finishing');
 }
 function _getDevices(t) {
   spark.listDevices().then(
@@ -85,10 +74,10 @@ function _getDevices(t) {
       devices.forEach(function(device) {
         var keys = Object.keys(t.knownDevices);
         var match = false;
-        for(var key in keys) {
-          if(keys[key] === device.name) {
+        for(key in keys) {
+          if(keys[key] == device.name) {
             match = true;
-          }
+          } 
         }
         // we know about the device
         if(match) {
@@ -96,30 +85,30 @@ function _getDevices(t) {
           if(t.knownDevices[device.name].connected) {
             // we only really care if it changest state
             if(!device.connected) {
-              debug("[DeviceList] Device has gone offline %s", device.name);
+              debug('[DeviceList] Device has gone offline %s',device.name);
               t.knownDevices[device.name] = device;
             }
           } else {
             // It wasn't connected before, did it come back online
             if(device.connected) {
               // it came back!
-              debug("[DeviceList] Device has come back online %s", device.name);
+              debug('[DeviceList] Device has come back online %s',device.name);
               t.knownDevices[device.name] = device;
-            }
+            } 
           }
         } else {
           // We haven't seen the device before
           t.knownDevices[device.name] = device;
           // is it online or not?
           if(device.connected) {
-            debug("[DeviceList] New online device %s", device.name);
+            debug('[DeviceList] New online device %s',device.name);
           } else {
-            debug("[DeviceList] New offline device %s", device.name);
+            debug('[DeviceList] New offline device %s',device.name);
           }
         }
       });
     }, function(error) {
-        debug("[DeviceList] Failure getting devices %s", error);
+        debug('[DeviceList] Failure getting devices %s',error);
     }
   );
 }
